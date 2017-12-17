71cc89ce8f9e953fd04784f1ea2721dbcbd58327
Jacob <jmooree30@gmail.com>
2017-12-17 01:10:06 -0500

    I hate heroku

--- db/migrate/20171217050724_remove_colums_from_friendships.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumsFromFriendships < ActiveRecord::Migration[5.1]
-  def change
-  	remove_reference :friendships, :friend
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217050724) do
+ActiveRecord::Schema.define(version: 20171217055455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20171217050724) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "friend_id"
+    t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
