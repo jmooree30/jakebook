160dc5d82da4dbca86f480b99079b0b14fa5bec0
Jacob <jmooree30@gmail.com>
2018-01-01 14:13:35 -0500

    jquery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //= require jquery
 //= require jquery_ujs
-
+//= require bootstrap
 //= require bootstrap/collapse
 //= require bootstrap/dropdown
 //= require turbolinks
