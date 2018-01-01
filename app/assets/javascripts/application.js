085ff609dd456e2b95e7f178d87989f0a6eefc27
Jacob <jmooree30@gmail.com>
2018-01-01 14:01:28 -0500

    jquery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery
-//= require jquery_ujs
+
 //= require bootstrap
 //= require bootstrap/collapse
 //= require bootstrap/dropdown
